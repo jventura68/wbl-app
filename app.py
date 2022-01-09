b356fb6e45a70fa19423f36bfe6989259bf2cc9a
jventura68 <jose.ventura.roda@gmail.com>
2022-01-09 13:31:25 +0100

    First map

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+df = pd.read_excel('data/wbl_regime.csv')
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
+app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+    html.P("Gobiernos:"),
+    dcc.RadioItems(
+        id='elected', 
+        options=[{'value': 'Democrático', 'label': True},
+                  'value': 'No elegido', 'label': False}],
+        value=True,
+        labelStyle={'display': 'inline-block'}
     ),
-    html.Div(id='display-value')
+    dcc.Graph(id="choropleth"),
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-                [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+@app.callback(
+    Output("choropleth", "figure"), 
+    [Input("elected", "value")])
+def display_choropleth(elected):
+    fig = px.choropleth(df, 
+                    locations="iso3c", 
+                    color="WBL INDEX",
+                    hover_name="economy",
+                    animation_frame="reportyr",
+                    color_continuous_scale=['red', 'yellow', 'green'],
+                    range_color=[20, 100]
+    )
+    fig.update_geos(
+        projection_type="natural earth",
+        visible=False, 
+        showcountries=True, countrycolor="black",
+        showland=True, landcolor="#CCCCCC"
+    )
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    fig.update_layout(height=500, margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    return fig
+
+app.run_server(debug=True)
